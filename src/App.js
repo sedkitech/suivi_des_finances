6f7cc442b28bc2e84f581c012bed1e3ec9611215
sedkitech <dhiasedkipro@gmail.com>
2022-12-11 14:43:40 +0100

    setting firestore rules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 //components and pages
 import Navbar from './components/Navbar';
